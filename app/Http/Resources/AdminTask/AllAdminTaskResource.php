db8853991c78e31892e8b463ba6d45f043905ea7
MohamedHassan1996 <mohamedhassan00096@gmail.com>
2025-02-16 23:00:06 +0200

    updated

--- app/Http/Resources/AdminTask/AllAdminTaskResource.php
+++ app/Http/Resources/AdminTask/AllAdminTaskResource.php
@@ -47,7 +47,7 @@ class AllAdminTaskResource extends JsonResource
         $formattedEndTime = "";
 
         if($endTime[0]->task_id == 1203){
-            dd($endTime[0]->status != 0);
+            dd(count($endTime) == 1);
         }
 
 
