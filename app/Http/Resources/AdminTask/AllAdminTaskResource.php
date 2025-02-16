40c2583593e4406e3158190e60c2699462cdd779
MohamedHassan1996 <mohamedhassan00096@gmail.com>
2025-02-16 23:05:12 +0200

    updated

--- app/Http/Resources/AdminTask/AllAdminTaskResource.php
+++ app/Http/Resources/AdminTask/AllAdminTaskResource.php
@@ -47,7 +47,7 @@ class AllAdminTaskResource extends JsonResource
         $formattedEndTime = "";
 
         if(count($endTime) == 1){
-            dd("r");
+            dd($endTime[0]->status->value);
             if($endTime[0]->status->value != 0){
                 dd("r");
                 $formattedEndTime = Carbon::parse($endTime[0]->created_at)->format('d/m/Y H:i:s');
