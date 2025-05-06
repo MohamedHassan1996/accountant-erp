658827e50056940f6b40538eeac203501c111ec7
MohamedHassan1996 <mohamedhassan00096@gmail.com>
2025-05-06 18:59:52 +0300

    t

--- app/Services/Select/Invoice/InvoiceSelectService.php
+++ app/Services/Select/Invoice/InvoiceSelectService.php
@@ -3,12 +3,21 @@
 namespace App\Services\Select\Invoice;
 
 use App\Models\Invoice\Invoice;
+use Illuminate\Support\Facades\DB;
 
 class InvoiceSelectService
 {
-    public function getAllInvoices()
+    public function getAllInvoices(?int $clientId = null)
     {
-        return Invoice::select(['id as value', 'number as label'])->get();
+        return Invoice::select([
+                DB::raw("CONCAT(number, ' - ', DATE_FORMAT(created_at, '%d/%m/%Y')) as value"),
+                'number as label'
+            ])
+            ->when($clientId !== null, function ($query) use ($clientId) {
+                $query->where('client_id', $clientId);
+            })
+            ->get();
     }
+
 }
 
