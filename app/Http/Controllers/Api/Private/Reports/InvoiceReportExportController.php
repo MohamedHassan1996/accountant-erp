91646b202507bdde64a41419558bea6c61679c47
MohamedHassan1996 <mohamedhassan00096@gmail.com>
2025-05-09 14:32:13 +0300

    t

--- app/Http/Controllers/Api/Private/Reports/InvoiceReportExportController.php
+++ app/Http/Controllers/Api/Private/Reports/InvoiceReportExportController.php
@@ -238,7 +238,7 @@ class InvoiceReportExportController extends Controller
                 ? $invoiceItemData->serviceCategory->name
                 : $invoiceItemData->parameterValue?->description ?? $invoiceItem->description;
 
-            dd($invoiceItemData);
+            dd($invoiceItem->description);
 
 
             $invoiceItemsData[] = [
