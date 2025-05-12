dec93151f1177664bcaee02f647c32bdddb43813
MohamedHassan1996 <mohamedhassan00096@gmail.com>
2025-05-12 04:38:05 +0300

    t

--- app/Http/Controllers/Api/Private/Invoice/RecurringInvoiceController.php
+++ app/Http/Controllers/Api/Private/Invoice/RecurringInvoiceController.php
@@ -55,7 +55,7 @@ class RecurringInvoiceController extends Controller
                 $allowedDaysToPay = $client->allowed_days_to_pay ?? 0; // Fetch from the client table
 
                 $startDate = Carbon::parse($payInstallmentData['startAt']);
-                $endDate = $startDate->copy()->addMonths($clientEndDataAddMonth);
+                $endDate = $startDate->copy()->addMonths($clientEndDataAddMonth)->subDays(1);
 
 
                 $isSpecialMonthEnd = in_array($endDate->format('m-d'), ['08-31', '12-31']);
