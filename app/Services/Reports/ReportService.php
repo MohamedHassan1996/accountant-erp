7fee031bf426da0944429cbbd56b42d1410eadbe
MohamedHassan1996 <m.hassan@elmotech.it>
2026-06-07 14:26:01 +0300

    Update ReportService.php

--- app/Services/Reports/ReportService.php
+++ app/Services/Reports/ReportService.php
@@ -1,38 +1,34 @@
 <?php
 namespace App\Services\Reports;
+
 use App\Enums\Task\TaskStatus;
-use App\Models\Client;
+use App\Models\Client\Client;
 use App\Models\Task\Task;
-use App\Models\Invoice\Invoice;
-use Illuminate\Support\Facades\DB;
-
-
 
 class ReportService
 {
-     public function reports()
+    public function reports()
     {
-        $authUser=auth()->user();
-        $clients=DB::table('clients')->count();
-        $invoiced =Task::whereNotNull('invoice_id')->count();
-        $notInvoiced =Task::where('invoice_id',null)->count();
-        $toWork =Task::where('status',TaskStatus::TO_WORK->value)->where('user_id',$authUser->id)->count();
-        $inProgress =Task::where('status',TaskStatus::IN_PROGRESS->value)->where('user_id',$authUser->id)->count();
-        $done =Task::where('status',TaskStatus::DONE->value)->where('user_id',$authUser->id)->count();
+        $authUser = auth()->user();
+
+        $clients = Client::whereNull('deleted_at')->count();
+        $invoiced = Task::whereNull('deleted_at')->whereNotNull('invoice_id')->count();
+        $notInvoiced = Task::whereNull('deleted_at')->whereNull('invoice_id')->count();
+        $toWork = Task::whereNull('deleted_at')->where('status', TaskStatus::TO_WORK->value)->where('user_id', $authUser->id)->count();
+        $inProgress = Task::whereNull('deleted_at')->where('status', TaskStatus::IN_PROGRESS->value)->where('user_id', $authUser->id)->count();
+        $done = Task::whereNull('deleted_at')->where('status', TaskStatus::DONE->value)->where('user_id', $authUser->id)->count();
+
         return response()->json([
-            "clients"=>$clients,
-            "invoices"=>[
-                "invoiced"=>$invoiced,
-                "notInvoiced"=>$notInvoiced
+            "clients" => $clients,
+            "invoices" => [
+                "invoiced" => $invoiced,
+                "notInvoiced" => $notInvoiced
             ],
-            "tasks"=>[
-                "toWork"=>$toWork,
-                "inProgress"=>$inProgress,
-                "done"=>$done,
+            "tasks" => [
+                "toWork" => $toWork,
+                "inProgress" => $inProgress,
+                "done" => $done,
             ]
-          ],200);
-
-
+        ], 200);
     }
 }
-
